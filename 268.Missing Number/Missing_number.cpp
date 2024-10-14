f7af5cf7414b791b0e0264979e3c93474da34ab5
Pratik Singh <pratikkumars623@gmail.com>
2024-10-14 22:39:20 +0530

    some problems solved from LEET CODE

---
+++ 268.Missing Number/Missing_number.cpp
@@ -0,0 +1,18 @@
+//LEET CODE SOlution-
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int sum=0;
+//         int n=nums.size();
+//         for(int index=0; index<n; index++)
+//         {
+//             sum+=nums[index];
+//         }
+//         int totalsum=(n*(n+1))/2;
+//         int ans=totalsum-sum;
+//         return ans;
+        
+//     }
+// };
+
