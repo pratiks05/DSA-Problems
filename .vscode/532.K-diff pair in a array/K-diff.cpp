e105a6382c39f954528cdca9c1c1d834f8e32434
Pratik Singh <pratikkumars623@gmail.com>
2025-01-06 21:16:21 +0530

    New Problem added

---
+++ .vscode/532.K-diff pair in a array/K-diff.cpp
@@ -0,0 +1,30 @@
+//Leet Code Solution
+//Concept- Two Pointer Approch
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+      sort(nums.begin(),nums.end());
+      set<pair<int,int>>ans;
+      int i=0,j=1;
+      while(j<nums.size())
+      {
+        int diff=nums[j]-nums[i];
+        if(diff==k)
+        {
+            ans.insert({nums[i],nums[j]});
+            i++,j++;
+        }
+        else if(diff<k)
+        {
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+        if(i==j) j++;
+      }
+      return ans.size();
+    }
+};
--- 75.Sort Colors/sort_colors.cpp
+++ 75.Sort Colors/sort_colors.cpp
@@ -84,7 +84,7 @@ int main()
 
 //APPROCH AND LOGIC-
 
-// Two pointer approch is followed and three pointers are used here
-//  index pointer for array traversal left pointer for storing 0's and right pointer for storing 2's
+//Two pointer approch is followed and three pointers are used here
+//index pointer->for array traversal , left pointer-> for storing 0's , and right pointer-> for storing 2's
 //while loop is used with condition index<=right because it is expected that after right pointer 2's will be there
 //the catch is this that we don't need to increase the index(index++) after right--
